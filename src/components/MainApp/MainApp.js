2e71c2a1732b891a366326f9e26aa0226180661f
annhoang191 <hquynhanh19@gmail.com>
2019-05-08 08:20:08 +0700

    Organizes code folder structures

---
+++ src/components/MainApp/MainApp.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.css';
+
+function App() {
+  return (
+    <div className="App">
+        <h2>Your trustworthy comic information booth</h2>
+    </div>
+  );
+}
+
+export default App;
