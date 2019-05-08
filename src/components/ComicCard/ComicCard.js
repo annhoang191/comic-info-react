24289948b2bdefb98be19f557f9dd0b31166fce1
annhoang191 <hquynhanh19@gmail.com>
2019-05-08 15:43:46 +0700

    Fetches from Comic Vine API

--- src/components/ComicCard/ComicCard.js
+++ src/components/ComicCard/ComicCard.js
@@ -3,29 +3,28 @@ import './style.css';
 
 class ComicCard extends Component {
   render() {
-    const {name, genre, thumbnail, author, review, summary} = this.props;
-
+    const {
+      name,
+      cover_date,
+      thumbnail,
+      summary,
+      site_detail_url
+    } = this.props;
     return(
-      <div className="col-md-4">
+      <div className="col-md-3">
         <div className="card-view">
           <div className="thumbnail-image">
             <img src={thumbnail} alt="thumbnail" />
           </div>
           <div className="container">
-            <a>
-              <div className="tag_name red">{genre}</div>
-            </a>
+            <div className="tag_name red">{cover_date}</div>
             <div className="comic_info">
               <div className="info">
-                <a className="name">{name}</a><br/>
-                <span className="author">{author}</span><br/>
+                <a className="name" href={site_detail_url}>{name}</a><br/>
               </div>
               <hr />
               <div className="content">
-                <a className="review">
-                  <p>{review}</p>
-                </a>
-                <p className="summary">{summary}</p>
+                <p className="summary" dangerouslySetInnerHTML={{__html: summary}} />
               </div>
             </div>
           </div>
--- src/components/MainApp/MainApp.js
+++ src/components/MainApp/MainApp.js
@@ -1,44 +1,38 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
 import './style.css';
 
 import NavBar from '../NavBar/NavBar';
 import ComicCard from '../ComicCard/ComicCard';
 
-const comics = [
-  {
-    objectID: 0,
-    name: 'Saga',
-    genre: 'Epic space opera/fantasy',
-    thumbnail: 'https://cdn.imagecomics.com/assets/i/releases/2453/saga-44_13c8c1c991.jpg',
-    author: 'Brian K. Vaughan, Fiona Staples',
-    review: 'This is an original fantasy book with no superheroes, two non-white leads and an opening chapter featuring graphic robot sex. I thought we might be cancelled by our third issue.',
-    summary: 'Saga is an ongoing series and space opera from Brian K. Vaughan and Fiona Staples described as Romeo and Juliet meets Star Wars meets Game of Thrones...'
-  },
-  {
-    objectID: 1,
-    name: 'Paper Girls',
-    genre: 'Mystery',
-    thumbnail: 'https://66.media.tumblr.com/a4190c6a885047919592d2fd9704a238/tumblr_inline_nw6nz1kgC31tceg4s_500.jpg',
-    author: 'Brian K. Vaughan, Cliff Chiang',
-    review: 'THE PERFECT COMIC FOR YOUR \'80S NOSTALGIA TRIP',
-    summary: 'an ongoing sci-fi adventure series following four newspaper delivery girls as they wind up in the middle of a time travel mystery'
-  },
-];
+import {
+  PATH_BASE,
+  PATH_CORS,
+  API_KEY
+} from '../../constants/index';
 
+let url = `${PATH_CORS}${PATH_BASE}?api_key=${API_KEY}&format=json&filter=name:Saga&sort=cover_date:desc`;
 
 class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      comics: comics
+      comics: []
     }
-
-    this.onSearchChange = this.onSearchChange.bind(this);
   }
 
-  onSearchChange(event) {
-    this.setState({searchTerm: event.target.value});
+
+  fetchComics(){
+    axios
+    .get(url)
+    .then(response => this.setState({comics: response.data.results}))
+    .catch(errors => console.log(errors));
+  }
+
+  componentDidMount() {
+    this.fetchComics();
   }
 
   render() {
@@ -53,15 +47,14 @@ class App extends Component {
           </div>
         </div>
 
-        <div className="list-comics row container">
+        <div className="list-comics row">
           {comics.map(comic =>
-            <ComicCard key={comic.objectID}
+            <ComicCard key={comic.id}
               name={comic.name}
-              thumbnail={comic.thumbnail}
-              genre={comic.genre}
-              author={comic.author}
-              review={comic.review}
-              summary={comic.summary}/>
+              thumbnail={comic.image.thumb_url}
+              cover_date={comic.cover_date}
+              site_detail_url={comic.site_detail_url}
+              summary={comic.description}/>
           )}
         </div>
       </div>
